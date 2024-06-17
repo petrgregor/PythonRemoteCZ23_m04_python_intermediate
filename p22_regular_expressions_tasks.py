daf3ba0f7d1433925fcf2c9926bcd5d36b6bd440
Petr Gregor <petr.gregor@email.cz>
2024-06-17 13:30:03 +0200

    Regular expressions - HW.

--- p22_regular_expressions_tasks.py
+++ p22_regular_expressions_tasks.py
@@ -61,6 +61,8 @@ Task 3a:
 Pomocí regulárního výrazu rozhodněte, zda je zadané číslo dělitelné 10.
 """
 print(task3a)
+numbers = "123,100,50,354,35640,546414321654310,0,+150,-150,54,-54,+54"
+list(map(lambda x: print(f"{x}: {bool(re.fullmatch(r"[+-]?\d*0", x))}"), numbers.split(",")))
 
 
 print('-'*80)
@@ -68,10 +70,21 @@ task3b = """
 Task3b:
 Pomocí regulárního výrazu rozhodněte, zda je zadané číslo sudé.
 """
+print(task3b)
+numbers = "123,100,50,354,35640,546414321654310,0,+150,-150,54,-54,+54,-53,53,+53"
+list(map(lambda x: print(f"{x}: {bool(re.fullmatch(r"[+-]?\d*[02468]", x))}"), numbers.split(",")))
 
-
+print('-'*80)
 task4 = """
 Task 
 Pomocí regulárního výrazu ověřte, zda je zadané číslo platným rodným číslem.
 Dělitelnost číslem 11 nelze řešit regulárním výrazem, to musíme v Pythonu.
 """
+print(task4)
+numbers = "850315/4563,515225/236,000101/0001,1236/123,802325/4563,851245/4563,996512/4563,995512/4563,200101/1235"
+
+# FIXME: zatím neřeší různě dlouhé měsíce - to regulárním výrazem jde, ale je to komplikované, řešil bych v Pythonu
+# FIXME: zatím neřeší trojčíslí na konci pouze před určitým rokem (regulárním výrazem nejde - jedině pak v Pythonu)
+# FIXME: dělitelnost číslem 11 nelze řešit regulárním výrazem, to musíme v Pythonu.
+pattern = r"\d{2}(0[1-9]|10|11|12|5[1-9]|60|61|62)(0[1-9]|[12]\d|30|31)/\d{3,4}"
+list(map(lambda x: print(f"{x}: {bool(re.fullmatch(pattern, x))}"), numbers.split(",")))
---
+++ p23_regular_expressions_HW.py
@@ -0,0 +1,22 @@
+"""
+Task
+
+Načtěte soubor: Jules_Verne_-_Twenty_thousand_leagues_under_the_sea.txt,
+který obsahuje celý anglický text knihy od Jules Verna.
+
+Zdroj: https://gutenberg.org/ebooks/164
+       https://gutenberg.org/ebooks/164.txt.utf-8
+
+Od sekce FIRST PART na řádku 308 (včetně) do "END OF THE SECOND PART" na řádku 18011
+zpracujte následující úkoly:
+- spočítejte počet vět
+- spočítejte počet slov
+- najděte všechna čísla (vypište počet hodnot)
+- najděte všechny letopočty (vypište je)
+- vypište všechna slova před slovem "kilometers"
+- vypište všechna jména - každé pouze jednou (pro jednoduchost všechna slova začínající velkým písmenem a nejsou
+na začátku řádku)
+- kolikrát se v textu vyskytuje slovo "Nautilus"
+- kolik má kniha kapitol (CHAPTER)?
+- vypište názvy kapitol (první věta po CHAPTER #)
+"""
